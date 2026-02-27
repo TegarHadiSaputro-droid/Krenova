93f3efb3cec72bdce651a24e7cf3bb42e29d83f8
egiii <Egaaakahh@gmail.com>
2026-02-27 07:26:18 +0700

    initial commit Krenova

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="s.css">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+    body{
+        background-color: #f0f0f0;
+        font-family: Arial, sans-serif;
+        text-align: center;
+        padding: 50px;
+    }
+    
+    </style>
+ <header>
+     <h1>Welcome to Laravel</h1>
+
+     <p>indonesia negeri aman</p>
+ </header>  
+
+</body>
+</html> 
